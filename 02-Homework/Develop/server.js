714aa35758f870ce1ad7022df2fe1270f28107b2
Juan Vazquez <ehulises28@hotmail.com>
2021-04-22 21:23:26 -0700

    added html routes

--- 02-Homework/Develop/server.js
+++ 02-Homework/Develop/server.js
@@ -1,5 +1,4 @@
 //dependencies
-const http = require("http");
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
@@ -7,8 +6,26 @@ const util = require("util");
 
 //sets up the express app
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 //set up Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+
+//Routes for html
+app.get("/,", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+//rotes for api
+const readFilePromise = util.promisify(fs.readFile);
+const writeFilePromise = util.promisify(fs.writeFile);
+
+//LISTENER
+//code below starts our server
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
